f313078eca83e0ce66ed0379d389bbf8cc3c0b8c
ALUSTADYT <115578168+ALUSTADYT@users.noreply.github.com>
2025-09-08 10:49:43 +0500

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -35,8 +35,8 @@ async (conn, mek, m, { from, sender, reply }) => {
                 forwardingScore: 1000,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: '𝓐𝓻𝓼𝓵𝓪𝓷_𝓜𝓓',
+                    newsletterJid: '120363318478753709@newsletter',
+                    newsletterName: 'E3-WAQAR-MD',
                     serverMessageId: 143
                 }
             }
